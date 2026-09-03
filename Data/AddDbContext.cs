cc8fac671a8faa5ad0d2178fb18300489c27f5c8
Kalaiyarasi Surenthiran <UT011685@unicomtic.com>
2026-09-03 10:17:45 +0530

    Implement authentication module with JWT

--- Data/AddDbContext.cs
+++ Data/AddDbContext.cs
@@ -1,6 +1,23 @@
-﻿namespace PersonalDigitalVaultSystem.Data
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalDigitalVaultSystem.Models;
+
+namespace PersonalDigitalVaultSystem.Data
 {
-    public class AddDbContext
+    public class AddDbContext : DbContext
     {
+        public AddDbContext(DbContextOptions<AddDbContext> options) : base(options) { }
+
+        public DbSet<ApplicationUser> Users => Set<ApplicationUser>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>(entity =>
+            {
+                entity.HasIndex(u => u.Username).IsUnique();
+                entity.HasIndex(u => u.Email).IsUnique();
+            });
+        }
     }
 }
